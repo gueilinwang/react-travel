935ee8fe21562854eb552a15004d939c9f58dabf
gueilinwang <cool4003213@gmail.com>
2021-10-20 01:57:45 +0800

    使用react-redux
    範例:在class component中使用react redux提供的connect hoc

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,52 +4,54 @@ import logo from "../../assets/logo.svg"
 import { Layout, Typography, Input, Menu, Button, Dropdown } from "antd"
 import { GlobalOutlined } from "@ant-design/icons"
 import { withRouter, RouteComponentProps } from "react-router-dom"
-import store from "../../redux/store"
-import { ILanguageState } from "../../redux/language/languageReducer"
+import { RootState } from "../../redux/store"
 import { withTranslation, WithTranslation } from "react-i18next"
 import {
   changeLanguageActionCreator,
   addLanguageActionCreator,
 } from "../../redux/language/languageActions"
-interface IHeaderState extends ILanguageState {}
-class HeaderComponent extends React.Component<
-  RouteComponentProps & WithTranslation,
-  IHeaderState
-> {
-  constructor(props) {
-    super(props)
-    const storeState = store.getState()
-    this.state = {
-      language: storeState.language,
-      languageList: storeState.languageList,
-    }
+import { connect } from "react-redux"
+import { Dispatch } from "redux"
+const mapStateToProps = (state: RootState) => {
+  return {
+    language: state.language,
+    languageList: state.languageList,
   }
+}
+
+const mapDispatchToProps = (dispatch: Dispatch) => {
+  return {
+    changeLanguage: (code: "zh-TW" | "en") => {
+      const action = changeLanguageActionCreator(code)
+      dispatch(action)
+    },
+    addLanguage: (name: string, code: string) => {
+      const action = addLanguageActionCreator(name, code)
+      dispatch(action)
+    },
+  }
+}
+type HeaderPropsType = RouteComponentProps & //react-router路由props類型
+  WithTranslation & //i18n props類型
+  ReturnType<typeof mapStateToProps> & //redux store映射類型
+  ReturnType<typeof mapDispatchToProps> //redux dispatch映射類型
+class HeaderComponent extends React.Component<HeaderPropsType> {
   menuClickHandler = (e) => {
     if (e.key === "new") {
       //處理新語言action
-      const action = addLanguageActionCreator("新語言", "new")
-      store.dispatch(action)
+      this.props.addLanguage("新語言", "new")
     } else {
       //處理切換語言action
-      const action = changeLanguageActionCreator(e.key)
-      store.dispatch(action)
+      this.props.changeLanguage(e.key)
     }
   }
-  handleStoreChange = () => {
-    const newState = store.getState()
-    this.setState({
-      language: newState.language,
-      languageList: newState.languageList,
-    })
-  }
+
   showNavigator = (data: string[]) => {
     return data.map((title, index) => {
       return <Menu.Item key={index}>{title}</Menu.Item>
     })
   }
-  componentDidMount() {
-    store.subscribe(this.handleStoreChange)
-  }
+
   render() {
     const { history, t } = this.props
     return (
@@ -62,7 +64,7 @@ class HeaderComponent extends React.Component<
                 style={{ marginLeft: 15 }}
                 overlay={
                   <Menu onClick={this.menuClickHandler}>
-                    {this.state.languageList.map((list) => {
+                    {this.props.languageList.map((list) => {
                       return <Menu.Item key={list.code}>{list.name}</Menu.Item>
                     })}
                     <Menu.Item key="new">
@@ -72,7 +74,7 @@ class HeaderComponent extends React.Component<
                 }
                 icon={<GlobalOutlined />}
               >
-                {this.state.language === "zh-TW" ? "中文" : "English"}
+                {this.props.language === "zh-TW" ? "中文" : "English"}
               </Dropdown.Button>
               <Button.Group className={styles["button-group"]}>
                 <Button onClick={() => history.push("register")}>
@@ -108,4 +110,7 @@ class HeaderComponent extends React.Component<
     )
   }
 }
-export const Header = withTranslation()(withRouter(HeaderComponent))
+export const Header = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withTranslation()(withRouter(HeaderComponent)))
