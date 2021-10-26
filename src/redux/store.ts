7094ee18596f6b8d5be8d00efc6b0a4d99b75000
gueilinwang <cool4003213@gmail.com>
2021-10-27 01:25:45 +0800

    在store中引入languageChange中間件組件

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,12 +3,16 @@ import languageReducer from "./language/languageReducer"
 import recommendProductsReducer from "./recommendProducts/recommendProductsReducer"
 import thunk from "redux-thunk"
 import { actionLog } from "./middlewares/actionLog"
+import { languageChange } from "./middlewares/languageChange"
 
 const rootReducer = combineReducers({
   language: languageReducer,
   recommendProducts: recommendProductsReducer,
 })
-const store = createStore(rootReducer, applyMiddleware(thunk, actionLog))
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk, actionLog, languageChange)
+)
 
 export type RootState = ReturnType<typeof store.getState>
 
