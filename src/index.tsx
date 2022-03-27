8449a762c8a749c607285c8aff44994408cc416e
gueilinwang <cool4003213@gmail.com>
2022-03-27 22:43:06 +0800

    更新token

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import { Provider } from "react-redux"
 import rootStore from "./redux/store"
 import axios from "axios"
 import { PersistGate } from "redux-persist/integration/react"
-axios.defaults.headers["x-icode"] = "02BE11D2BDEFD5C3"
+axios.defaults.headers["x-icode"] = "3FB94394D52974F1"
 ReactDOM.render(
   <Provider store={rootStore.store}>
     <PersistGate persistor={rootStore.persistor}>
